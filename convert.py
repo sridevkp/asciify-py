0623818dfcf7dab9041a8e369226272d48e75e36
sridevkp <69075577+sridevkp@users.noreply.github.com>
2024-02-27 23:10:00 +0530

    finished

--- convert.py
+++ convert.py
@@ -3,8 +3,8 @@ import time
 from PIL import Image, ImageOps
 
 MAX_SIZE = 200 
-CHARS = [ ".",",","*","^","#","$","%","&" ]
-STEP = 255 / len(CHARS) 
+CHARS = [ " ",".",",","*","^","#","%","$","&" ]
+STEP = 255 / (len(CHARS) -1)
 
 [ filename, image_file_path, ascii_out_file_path ] = sys.argv
 
@@ -14,15 +14,14 @@ width, height = raw_img.size
 scale_f = MAX_SIZE/ max( width, height )
 
 width  = int(width  * scale_f)
-height = int(height * scale_f * .5)
+height = int(height * scale_f *.5)
 
 raw_img = raw_img.resize( ( width, height ) )
 gs_img = ImageOps.grayscale( raw_img )
-gs_img.show()
+
 px = gs_img.load()
 
 txt = ""
-
 
 
 for y in range(height):
